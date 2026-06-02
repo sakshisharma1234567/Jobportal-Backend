8f96f393e6a0cbf2f37edf78f2e3e9a926a83046
sakshisharma1234567 <sakshu9650@gmail.com>
2026-06-02 15:37:50 +0530

    Refactor applicationRoutes.js and update Cloudinary settings
    Removed commented-out code and streamlined imports. Updated Cloudinary resource type to 'auto'.

--- routes/applicationRoutes.js
+++ routes/applicationRoutes.js
@@ -1,71 +1,3 @@
-// const express = require("express");
-// const fs = require("fs");
-// const multer = require("multer");
-
-// const {
-//   applyJob,
-//   getStudentApplications,
-//   getRecruiterApplications
-// } = require(
-//   "../controllers/applicationController"
-// );
-
-// const uploadDir = "uploads";
-// if (!fs.existsSync(uploadDir)) {
-//   fs.mkdirSync(uploadDir, { recursive: true });
-// }
-
-// const storage = multer.diskStorage({
-//   destination: (req, file, cb) => {
-//     cb(null, uploadDir);
-//   },
-//   filename: (req, file, cb) => {
-//     const timestamp = Date.now();
-//     const safeName = `${timestamp}-${file.originalname}`.replace(/\s+/g, "-");
-//     cb(null, safeName);
-//   }
-// });
-
-// const upload = multer({ storage });
-
-// const router = express.Router();
-
-// // Apply Job
-
-// router.post(
-//   "/",
-//   upload.single("resume"),
-//   applyJob
-// );
-
-// // Student Applications
-
-// router.get(
-//   "/student/:id",
-//   getStudentApplications
-// );
-
-// // Recruiter Applications
-
-// router.get(
-//   "/recruiter/:id",
-//   getRecruiterApplications
-// );
-
-// // Test Route
-
-// router.get(
-//   "/test",
-//   (req, res) => {
-
-//     res.send(
-//       "Applications Route Working"
-//     );
-
-//   }
-// );
-
-// module.exports = router;
 
 const express = require("express");
 const multer = require("multer");
@@ -93,9 +25,9 @@ new CloudinaryStorage({
   cloudinary,
 
   params: {
-    folder: "resumes",
-    resource_type: "raw"
-  }
+  folder: "resumes",
+  resource_type: "auto"
+}
 
 });
 
